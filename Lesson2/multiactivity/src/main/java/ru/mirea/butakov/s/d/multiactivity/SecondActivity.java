042039008e8b7b43d11a951a829a04adea1664d6
BloodyImp <flamberg100@gmail.com>
2023-04-07 00:08:31 +0300

    загрузка 1й и 2й практики

---
+++ Lesson2/multiactivity/src/main/java/ru/mirea/butakov/s/d/multiactivity/SecondActivity.java
@@ -0,0 +1,26 @@
+package ru.mirea.butakov.s.d.multiactivity;
+
+import android.os.Bundle;
+import com.google.android.material.snackbar.Snackbar;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import ru.mirea.butakov.s.d.multiactivity.databinding.ActivitySecondBinding;
+
+public class SecondActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        String text = (String) getIntent().getSerializableExtra("key");
+        setContentView(R.layout.activity_second);
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(text);
+    }
+}
---
+++ Lesson2/toastapp/src/main/java/ru/mirea/butakov/s/d/toastapp/MainActivity.java
@@ -0,0 +1,26 @@
+package ru.mirea.butakov.s.d.toastapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onClickGetToast(View view) {
+        TextView textView = findViewById(R.id.editTextTextPersonName);
+        int count = textView.getText().length();
+        Toast toast = Toast.makeText(getApplicationContext(),
+                "студент №4 БСБО-17-20 Количество символов - " + count,
+                Toast.LENGTH_SHORT);
+        toast.setGravity(0, 0, 0);
+        toast.show();
+    }
+}
